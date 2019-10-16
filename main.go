fa6f12643e565efcf89935a97b59199c853316cf
Yeom <jiwony@a483e7428246.ant.amazon.com>
2019-10-16 18:57:57 +0900

    5th commit test

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello from the 4th commit!"}
+		res := &response{Message: "Hello from the 5th commit!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
