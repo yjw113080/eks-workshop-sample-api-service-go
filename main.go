162d119f3bb9eb4d96ef3ce55464949d43d6f62b
Yeom <jiwony@a483e7428246.ant.amazon.com>
2019-10-16 14:14:27 +0900

    last api test

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello from the second commit!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
